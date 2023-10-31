89e123c2c382ab7a3003a4f09756902a73a0b63c
semhal berhe <semhalberhe18@gmail.com>
2023-10-31 12:33:09 -0500

    app.ts added

---
+++ SD-415 WORKS/d12functions/src/app.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+    return a + b;
+   }
+   const sum1: number = add(10,15);
+   const sum2: number = add(9, 5 ) ;
+   console.log("sum1 is: ", sum1, " sum2 is: ", sum2);
