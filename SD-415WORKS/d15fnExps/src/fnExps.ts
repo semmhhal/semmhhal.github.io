10d15651c33df32a3711e3d21045cce821906102
semhal berhe <semhalberhe18@gmail.com>
2023-11-05 08:37:19 -0600

    commit

---
+++ SD-415WORKS/d15fnExps/src/fnExps.ts
@@ -0,0 +1,29 @@
+
+/**
+ *@returns {number} double the input
+ */
+ export function double(num: number): number {
+  return num * 2; 
+}
+
+
+/**
+ * @returns {number} 100 times the input
+ */
+export function times100(){
+ }
+
+
+/**
+ * @returns {Array} creates a new array with function mapped to each element
+ */
+export function myMap(  ){
+
+    return null;
+
+}
+
+
+
+
+
