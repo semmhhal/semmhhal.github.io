ccce3bc5a7472e2b84a905ffb76befe6d4a97c21
semhal berhe <semhalberhe18@gmail.com>
2023-11-05 10:16:45 -0600

    w1d5 items comitted

--- SD-415WORKS/d15fnExps/dist/src/fnExps.js
+++ SD-415WORKS/d15fnExps/dist/src/fnExps.js
@@ -1,34 +1,22 @@
-
 /**
  *@returns {number} double the input
  */
- export function double(num: number): number {
-  return num * 2; 
+export function double(num) {
+    return num * 2;
 }
-
-
 /**
  * @returns {number} 100 times the input
  */
-export function times100(num:number):number{
-  return num*100
- }
-
-
+export function times100(num) {
+    return num * 100;
+}
 /**
  * @returns {Array} creates a new array with function mapped to each element
  */
-export function myMap(arr:number[], callback:any):Object[] {
-  const result = [];
+export function myMap(arr, callback) {
+    const result = [];
     for (const ele of arr) {
         result.push(callback(ele));
     }
     return result;
-  }
-
-
-
-
-
-
-
+}
