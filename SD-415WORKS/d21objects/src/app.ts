598a415aa10b02e09ff9b6868f0511bdef7a6b07
semhal berhe <semhalberhe18@gmail.com>
2023-11-06 13:56:21 -0600

    base json added

---
+++ SD-415WORKS/d21objects/src/app.ts
@@ -0,0 +1,24 @@
+
+//use this file for any code you want to try out
+let Student:{studentID:number,quizAnswers:number[]}
+let student1={studentID:101,quizAnswers:[1,1,2,4]}
+let student2={studentID:101,quizAnswers:[2,1,2,2]}
+let student3={studentID:101,quizAnswers:[3,1,3,4]}
+
+console.log("expect 3: " computestudentScore(student1,[3,1,2,4]))
+//get the student score
+function computestudentScore(stu:Student,arr:number[]):number{
+    const answers=stu.quizAnswers;
+    //loop through the parallel arrays and count the matches and return that
+
+    let numCorrect=0;
+
+    for(let i=0;i<answers.length;i++){
+        if(answers[i]===quizKey[i]){
+            numCorrect=numCorrect+1
+        }
+    }
+    return numCorrect
+}
+student2;
+student3;
