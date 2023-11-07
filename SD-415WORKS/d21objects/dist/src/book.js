10184393a2ef38fc26248957463d296eabb02576
semhal berhe <semhalberhe18@gmail.com>
2023-11-07 13:26:25 -0600

    new

--- SD-415WORKS/d21objects/dist/src/book.js
+++ SD-415WORKS/d21objects/dist/src/book.js
@@ -1,85 +1,64 @@
 /*
-o	createBook, which will take title, author, and libraryID as inputs.  It will create a new book object and add it to the library, 
+o	createBook, which will take title, author, and libraryID as inputs.  It will create a new book object and add it to the library,
 which will be represented as a global array named libraryBooks.  createBook should return the newly created book.
 o	findTitles, which will find all the book titles in libraryBooks and return them in an alphabetically ordered array.
 o	findAuthors, which will find all the authors in libraryBooks and return them in an alphabetically ordered array.
 o	findIDs, which will find all the libraryIDs in libraryBooks and return them in an alphabetically ordered array.
 */
-
-export interface Book {
-    title: string,
-    author: string,
-    libraryID: number
-}
-
-export let library: Book[] = [
+export let library = [
     { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
     { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
     { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3245 }
 ];
-
 /**
  * Event handler to display library titles sorted alphabetically
  * @returns {undefined}
  */
-export function showTitles(): void {
+export function showTitles() {
     /* this function is complete, no need to modify.  Inspect it as an example for the other functions. */
     /* put all titles into an array, then sort, then join with newline and insert in textarea innerHTML */
-
     const titles = findTitles();
-
     /*need to sort and then join the titles still (e.g., someArray.join("\n")  */
     titles.sort();
     const titleString = titles.join("\n");
-
-    let textArea: HTMLInputElement | null = document.getElementById("displayArea") as HTMLInputElement | null;
-    if (textArea) { textArea.innerHTML = titleString; }
+    let textArea = document.getElementById("displayArea");
+    if (textArea) {
+        textArea.innerHTML = titleString;
+    }
 }
-
 /**
- * 
+ *
  * @return {object} array holding all titles as elements
  */
-export function findTitles(): string[] {
-
-
+export function findTitles() {
     //fix this to work according to the specs
-    let titles = ["This is a test title", "Another test title", "And another test title for good measure"];  
+    let titles = ["This is a test title", "Another test title", "And another test title for good measure"];
     return titles;
 }
-
-
 /**
  * @returns {undefined} no return
  * Event handler for Add book button.  Creates and adds book to the library
  */
-export function addBook():  void {
-
+export function addBook() {
     console.log("finish the implementation -- get the author, create a book object, and add to the library array!!");
 }
-
 /**
- * 
+ *
  * @returns {string[]} find all  authors in libraryBooks and return them in alphabetically ordered array.
  */
 // export function findAuthors(): string[] {
 //     //implement this
-
 // }
-
 /**
- * 
+ *
  * @returns {number[]} find all the libraryIDs in libraryBooks and return them in an alphabetically ordered array.
 */
 // export function findIDs(): number[] {
 //     //implement this
-
 // }
-
-
 /**
  * @returns {book} new book object
- * createBook, which will take title, author, and libraryID as inputs.  It will create a new book object and add it to the library, 
+ * createBook, which will take title, author, and libraryID as inputs.  It will create a new book object and add it to the library,
 which will be represented as a global array named libraryBooks.  createBook should return the newly created book.
  */
 //  
--- SD-415WORKS/d21objects/dist/src/quiz.js
+++ SD-415WORKS/d21objects/dist/src/quiz.js
@@ -1,60 +1,41 @@
-
-export interface StudentQuiz {
-    studentId: number,
-    quizAnswers: number[]
-}
-
-export function gradeStudent(studentQuizzes: number[], CORRECT_ANSWERS: number[]): number {
-let counter=0;
-for(let i=0;i<studentQuizzes.length;i++){
-    if(CORRECT_ANSWERS[i]===studentQuizzes[i]){
-        counter++
-    }
-}
-return counter
-}
-
-export function gradeQuiz(studentQuizzes: StudentQuiz[], CORRECT_ANSWERS: number[]): number[] {
-    let newarr: number[] = [];
-  
+export function gradeStudent(studentQuizzes, CORRECT_ANSWERS) {
+    let counter = 0;
     for (let i = 0; i < studentQuizzes.length; i++) {
-      let counter = 0;
-      let scores = studentQuizzes[i].quizAnswers;
-  
-      for (let j = 0; j < scores.length; j++) {
-        if (CORRECT_ANSWERS[j] === scores[j]) {
-          counter++;
+        if (CORRECT_ANSWERS[i] === studentQuizzes[i]) {
+            counter++;
         }
-      }
-  
-      newarr.push(counter);
     }
-  
+    return counter;
+}
+export function gradeQuiz(studentQuizzes, CORRECT_ANSWERS) {
+    let newarr = [];
+    for (let i = 0; i < studentQuizzes.length; i++) {
+        let counter = 0;
+        let scores = studentQuizzes[i].quizAnswers;
+        for (let j = 0; j < scores.length; j++) {
+            if (CORRECT_ANSWERS[j] === scores[j]) {
+                counter++;
+            }
+        }
+        newarr.push(counter);
+    }
     return newarr;
-  }
-
+}
 // export function gradeQuizLabeled(studentQuizzes: StudentQuiz[], CORRECT_ANSWERS: number[]): Object[]{
 //     interface StudentQ {
 //         studentId: number,
 //         quizAnswers: number
 //     }
 //     let newarr: StudentQ[] = [];
-  
 //     for (let i = 0; i < studentQuizzes.length; i++) {
 //       let counter = 0;
 //       let scores1 = studentQuizzes[i].quizAnswers;
-  
 //       for (let j = 0; j < scores1.length; j++) {
 //         if (CORRECT_ANSWERS[j] === scores1[j]) {
 //           counter++;
 //         }
 //       }
-  
 //       newarr.push({ studentId: studentQuizzes[i].studentId, quizAnswers: counter });
 //     }
-  
 //     return newarr;
 //   }
-
-
-
