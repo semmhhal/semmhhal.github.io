694becf57ec47b7f07173380842c9de732f53c24
semhal berhe <semhalberhe18@gmail.com>
2023-11-08 11:56:51 -0600

    files

--- SD-415WORKS/d22methods/src/app.ts
+++ SD-415WORKS/d22methods/src/app.ts
@@ -23,3 +23,27 @@ type Person = {
 
     manager.sayHi(); // Hello, my name is John. I am 27. My job is Software Engineer.'
  intern.sayHi(); // Hello, my name is Ben. I am 21. My job is Software Engineer Intern.
+
+////////
+
+ let x = 5;
+let y = {x};
+console.log(y); //{x : 5}
+function sum(arr: number[]): number[] { return arr }
+const foo = {sum,x };
+console.log(foo)
+console.log(foo.sum)
+console.log(foo.sum([1,2,3]))
+
+////////////write a for loop to print out the values of the properties according to the property names in the
+// propertyOrder array. You must use the properties array to access the values. You cannot directly
+// write console.log(numbers.one) …
+const numbers: { [key: string]: number } = {
+    one: 1,
+    two: 22,
+    three: 333,
+    four: 444,}
+   const propertyOrder = ["one", "four ", "three", "two", ];
+   for(const property of propertyOrder){
+   console.log(numbers[property])
+   }
