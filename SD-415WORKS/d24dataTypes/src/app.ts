d01b82d273e4f4fe053b8527b1ffcf83b7ade412
semhal berhe <semhalberhe18@gmail.com>
2023-11-10 10:49:15 -0600

    new

--- SD-415WORKS/d24dataTypes/src/app.ts
+++ SD-415WORKS/d24dataTypes/src/app.ts
@@ -3,45 +3,77 @@
  * 
  * @param {*} str 
  */
-export function ucFirst(str) {
+export function ucFirst(str:string):string {
+ let word=str.charAt(0).toUpperCase()
+  let newword =str.slice(1)
+  word+=newword
  
+
+return word
   }
 
   /**
    * 
    * @param {*} str 
    */
-  export  function checkSpam(str) {
- 
+  export  function checkSpam(str:string):boolean {
+ let newstr=str.toLowerCase()
+ return newstr.includes("buy viagra now")||newstr.includes("free xxxxx")
   }
 
-/**
- * 
- * @param {*} str 
- * @param {*} maxlength 
- */
-export   function truncate(str, maxlength) {
-
+// /**
+//  * 
+//  * @param {*} str 
+//  * @param {*} maxlength 
+//  */
+export   function truncate(str:string, maxlength:number):string {
+  let length=str.length
+  if(length>maxlength){
+    return str.slice(0,19)+"…"
   }
+  return str.slice(0,maxlength)
+}
 
 
-
-
-/**
- * 
- * @param {Array} arr of numbers
- * @returns {number} the total of the maximal subarray
+// /**
+//  * 
+//  * @param {Array} arr of numbers
+//  * @returns {number} the total of the maximal subarray
  
- */
-export function getMaxSubSum(arr) {
-
+//  */
+export function getMaxSubSum(arr:number[]):any{
+let maxnumber=0
+let sum=0
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]>maxnumber){
+    maxnumber=arr[i]
+    return maxnumber
+  }
+  else if(arr[i]>0){
+    sum+=arr[i]
+    return sum
+  }
+  
+  }
+  
 
 }
 
 
-export function camelize(str) {
-
+export function camelize(str:string):string {
+let newstr=""
+if(str===newstr){
+  return newstr
+}
+else if(
+  for(let i=0;i<str.charAt(i).length;i++){
+    if(str.charAt(i)==="-")
+  }
+)
   }
 
 
-export function extractCurrencyValue(){} 
+export function extractCurrencyValue(str:string):number{
+  let newel=+str.slice(1)
+  return newel
+} 
