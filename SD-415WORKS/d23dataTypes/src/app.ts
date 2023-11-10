971e2ffa5f6daedd68430bae64731add8e60ed35
semhal berhe <semhalberhe18@gmail.com>
2023-11-09 21:13:58 -0600

    new el

---
+++ SD-415WORKS/d23dataTypes/src/app.ts
@@ -0,0 +1,47 @@
+
+/**
+ * 
+ * @param {*} str 
+ */
+export function ucFirst(str) {
+ 
+  }
+
+  /**
+   * 
+   * @param {*} str 
+   */
+  export  function checkSpam(str) {
+ 
+  }
+
+/**
+ * 
+ * @param {*} str 
+ * @param {*} maxlength 
+ */
+export   function truncate(str, maxlength) {
+
+  }
+
+
+
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} the total of the maximal subarray
+ 
+ */
+export function getMaxSubSum(arr) {
+
+
+}
+
+
+export function camelize(str) {
+
+  }
+
+
+export function extractCurrencyValue(){} 
