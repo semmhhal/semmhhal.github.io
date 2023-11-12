84ae7079127f638b7b06b711acb06797ee36e1c3
semhal berhe <semhalberhe18@gmail.com>
2023-11-12 13:36:33 -0600

    new

---
+++ SD-415WORKS/d25practice/src/arrayPractice.ts
@@ -0,0 +1,39 @@
+
+export type Person = {
+    name: string,
+    age: number
+}
+
+export function doubleNums(arr){
+return arr.map(num => num * 2);
+}
+
+export function doubleAges(arr){
+
+}
+
+export function filterEven(arr){
+
+}
+
+export function filterOver10(arr) {
+
+}
+
+export function findEvenNum(arr){
+
+}
+
+export function findEvenAge(arr){
+
+}
+
+export function includesEvenNum(arr){
+
+}
+
+export function includesEvenAge(arr){
+
+}
+
+
