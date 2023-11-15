36ade30431941c4fa74ca9bc601f1a962fc9667a
semhal berhe <semhalberhe18@gmail.com>
2023-11-14 18:07:06 -0600

    new

---
+++ SD-415WORKS/d32destructure/src/destructure.ts
@@ -0,0 +1,15 @@
+/* 
+Create the function topSalary(salaries) that returns the name of the top-paid person.
+    If salaries is empty, it should return null.
+    If there are multiple top-paid persons, return any of them.
+P.S. Use Object.entries and destructuring to iterate over key/value pairs.
+*/
+
+// the following type definition says that SalaryObj has keys of type string and values of type number
+type SalaryObj = { [key: string]: number };
+
+
+
+
+
+ 
