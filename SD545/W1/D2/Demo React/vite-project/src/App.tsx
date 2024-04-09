6e186aed10245f047d3a7691f40682d2a36369f3
semhal berhe <semhalberhe18@gmail.com>
2024-04-08 19:09:21 -0500

    new

--- SD545/W1/D2/Demo React/vite-project/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import ListGroup from "./Components/ListGroup"
-
-
-function App(){
-  const name = 'Mosh'
-  return <div><ListGroup></ListGroup></div> //JSX 
-}
-
-export default App
--- SD545/W1/D2/Demo React/vite-project/src/Message.tsx
+++
@@ -1,7 +0,0 @@
-//PascalCasing
-function Message(){
-    const name = 'Semhal'
-    return <h1>Hello {name}</h1>
-}
-
-export default Message
